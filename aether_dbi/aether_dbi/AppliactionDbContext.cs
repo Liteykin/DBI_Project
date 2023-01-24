3b08e9ec91cb18a02953f94dbf85d893b42b49e6
Mark Liteykin <liteykin.mark@gmail.com>
2023-01-24 23:11:35 +0100

    Add files via upload
    C#

---
+++ aether_dbi/aether_dbi/AppliactionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aether_dbi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
