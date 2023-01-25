c532234738e739281e2b2768e09e4ccb771665c5
Mark Liteykin <lmark@duck.com>
2023-01-25 11:39:59 +0100

    C# Data upload works

--- aether_db/aether_db/Program.cs
+++ aether_db/aether_db/Program.cs
@@ -1,1 +1,6 @@
 ﻿using System;
+using var context = new aether_db.Models.aether_dbContext();
+var data = Console.ReadLine();
+context.DeviceTypes.Add(new aether_db.Models.DeviceType { DeviceType1 = data });
+context.SaveChanges();
+Console.WriteLine(data);
