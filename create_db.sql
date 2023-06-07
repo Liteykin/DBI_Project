2bdef1a85fab331a1bb0b85e35851b84f6b15157
Mark Liteykin <lmark@duck.com>
2023-06-07 22:27:05 +0200

    JSON Schema

--- create_db.sql
+++ create_db.sql
@@ -1,46 +1,80 @@
-CREATE DATABASE aether_db
-USE aether_db
+USE master;
+GO
+BEGIN
+    DECLARE @DBNAME AS VARCHAR(MAX) = 'Chatflow'
+    IF EXISTS(SELECT * FROM sys.databases WHERE Name = @DBNAME)
+    BEGIN
+        EXEC('ALTER DATABASE ' + @DBNAME + ' SET SINGLE_USER WITH ROLLBACK IMMEDIATE');
+        EXEC('DROP DATABASE ' + @DBNAME);
+    END;
+    EXEC('CREATE DATABASE ' + @DBNAME);
+END;
+USE Chatflow;
+GO
 
-CREATE TABLE Users (
-    UserID INT PRIMARY KEY IDENTITY (1,1),
-    Username VARCHAR(255) NOT NULL UNIQUE,
-    Name VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL UNIQUE,
-    Password VARCHAR(255) NOT NULL,
-    Number VARCHAR(255) NOT NULL UNIQUE,
-    created_at DATETIME,
-    last_login DATETIME
+CREATE TABLE [User](
+    UserId INT IDENTITY(1,1) PRIMARY KEY,
+    SupervisorId INT,
+    Username VARCHAR(50) NOT NULL,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    BirthDate DATE NOT NULL,
+    SignUpDate DATETIME NOT NULL,
+    Attributes VARCHAR(MAX),
+    CONSTRAINT [UQ_Email] UNIQUE (Email),
+    CONSTRAINT [FK_User_Supervisor] FOREIGN KEY (SupervisorId) REFERENCES [User](UserId),
+    CONSTRAINT [CHK_UserBirthDate] CHECK (BirthDate <= GETDATE())
 );
 
-CREATE TABLE DeviceTypes (
-    DeviceTypeID INT PRIMARY KEY IDENTITY (1,1),
-    DeviceType VARCHAR(255)
+CREATE TABLE [Category](
+    CategoryId INT IDENTITY(1,1) PRIMARY KEY,
+    CategoryName VARCHAR(50) NOT NULL,
+    CONSTRAINT [UQ_CategoryName] UNIQUE (CategoryName)
 );
 
-CREATE TABLE UserPreferences (
-    UserID INT PRIMARY KEY,
-    Language VARCHAR(255) NOT NULL,
-    Timezone VARCHAR(255) NOT NULL,
-    Theme VARCHAR(255) NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+CREATE TABLE [Room](
+    RoomId INT IDENTITY(1,1) PRIMARY KEY,
+    CategoryId INT NOT NULL,
+    RoomName VARCHAR(50) NOT NULL,
+    RoomCreationDate DATETIME NOT NULL,
+    CONSTRAINT [FK_Room_Category] FOREIGN KEY (CategoryId) REFERENCES [Category](CategoryId),
+    CONSTRAINT [UQ_Room_Category_RoomName] UNIQUE (CategoryId, RoomName)
 );
 
-CREATE TABLE Devices (
-    DeviceID INT PRIMARY KEY IDENTITY (1,1),
-    UserID INT NOT NULL,
-    DeviceType INT NOT NULL,
-    DeviceName VARCHAR(255),
-    FOREIGN KEY (UserID) REFERENCES Users(UserID),
-    FOREIGN KEY (DeviceType) REFERENCES DeviceTypes(DeviceTypeID)
+CREATE TABLE [UserRoom](
+    UserId INT NOT NULL,
+    RoomId INT NOT NULL,
+    UserRoomJoinDate DATETIME NOT NULL,
+    PRIMARY KEY (UserId, RoomId),
+    CONSTRAINT [FK_UserRoom_User] FOREIGN KEY (UserId) REFERENCES [User](UserId),
+    CONSTRAINT [FK_UserRoom_Room] FOREIGN KEY (RoomId) REFERENCES [Room](RoomId)
 );
 
-CREATE TABLE Commands (
-    CommandID INT PRIMARY KEY IDENTITY (1,1),
-    UserID INT NOT NULL,
-    DeviceID INT NOT NULL,
-    CommandText VARCHAR(255),
-    CommandResponse VARCHAR(255) NOT NULL,
-    Timestamp TIMESTAMP NOT NULL,
-    FOREIGN KEY (UserID) REFERENCES Users(UserID),
-    FOREIGN KEY (DeviceID) REFERENCES Devices(DeviceID)
+CREATE TABLE [Message](
+    MessageId INT IDENTITY(1,1) PRIMARY KEY,
+    UserId INT NOT NULL,
+    RoomId INT NOT NULL,
+    Text VARCHAR(1000) NOT NULL,
+    Timestamp DATETIME NOT NULL,
+    CONSTRAINT [FK_Message_User] FOREIGN KEY (UserId) REFERENCES [User](UserId),
+    CONSTRAINT [FK_Message_Room] FOREIGN KEY (RoomId) REFERENCES [Room](RoomId)
+);
+
+CREATE TABLE [ReactionType](
+    ReactionId INT IDENTITY(1,1) PRIMARY KEY,
+    ReactionType VARCHAR(50) NOT NULL,
+    CONSTRAINT [UQ_ReactionType] UNIQUE (ReactionType)
+);
+
+CREATE TABLE [MessageReaction](
+    MessageReactionId INT IDENTITY(1,1) PRIMARY KEY,
+    MessageId INT NOT NULL,
+    UserId INT NOT NULL,
+    ReactionId INT NOT NULL,
+    Timestamp DATETIME NOT NULL,
+    CONSTRAINT [FK_MessageReaction_Message] FOREIGN KEY (MessageId) REFERENCES [Message](MessageId),
+    CONSTRAINT [FK_MessageReaction_User] FOREIGN KEY (UserId) REFERENCES [User](UserId),
+    CONSTRAINT [FK_MessageReaction_ReactionType] FOREIGN KEY (ReactionId) REFERENCES [ReactionType](ReactionId)
 );
